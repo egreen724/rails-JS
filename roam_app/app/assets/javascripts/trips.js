eb6989c9524d05049a2e02275789f1e491ecbc8c
github username <github email address>
2019-08-24 17:09:11 -0400

    Adds logic to new trip display

--- roam_app/app/assets/javascripts/trips.js
+++ roam_app/app/assets/javascripts/trips.js
@@ -35,7 +35,15 @@ const tripFormEvents = () => {
       console.log(data)
       $('#app_container').html(" ")
       let newTrip = new Trip(data)
-      let html = newTrip.formatShow()
+      let html = ""
+      if (newTrip.taken === false) {
+        html = newTrip.toDoDisplay()
+      }
+
+      if (newTrip.taken === true) {
+        html = newTrip.tripTakenDisplay()
+      }
+
       $('#app_container').html(html)
     })
   })
@@ -54,9 +62,7 @@ function Trip(trip) {
   this.user = trip.user
 }
 
-Trip.prototype.formatShow = function() {
-  debugger
-  // if (this.taken === false) {
+Trip.prototype.toDoDisplay = function() {
     let tripHtml = `
       <h2> Activity Name: ${this.activity.name} </h2>
       <div class="info">
@@ -64,36 +70,30 @@ Trip.prototype.formatShow = function() {
       <h3> Distance: ${this.activity.distance}</h3>
       <h3> Address: ${this.activity.city}, ${this.activity.state}</h3>
       <a class="button" href="/trips/${this.id}/edit">Log a Trip</a>
-      <h2> ${this.user.name}'s Trip:</h2>
-      <h3> Date: ${this.date} </h3>
-      <h3> Time: ${this.time_ellapsed} hours</h3>
-      <h3> User Comments: ${this.comment}  </h3>
-      <a class="button" href="/activities/${this.activity_id}/trips">See similar Trips taken by other Users</a>
       <br>
       <a class="button" href="/trip/${this.id}/delete">Delete Trip</a>
       </div>
     `
-  //   return tripHtml
-  // } else (this.taken === false) {
-  //   let tripHtml = `
-  //   <h2> Activity Name: ${this.activity.name} </h2>
-  //   <div class="info">
-  //   <h3> Activity Category: ${this.activity.category}</h3>
-  //   <h3> Distance: ${this.activity.distance}</h3>
-  //   <h3> Address: ${this.activity.city}, ${this.activity.state}</h3>
-  //   <h2> ${this.user.name}'s Trip:</h2>
-  //   <h3> Date: ${this.date} </h3>
-  //   <h3> Time: ${this.time_ellapsed} hours</h3>
-  //   <h3> User Comments: ${this.comment}  </h3>
-  //   </div>
-  //   <a class="button" href="/activities/${this.activity_id}/trips">See similar Trips taken by other Users</a>
-  //   <a class="button" href="/trips/${this.id}/edit">Edit Trip Details</a>
-  //   <br>
-  //   <a class="button" href="/trip/${this.id}/delete">Delete Trip</a>
-  //   </div>
-  //   `
-  //   return tripHtml
-  // }
-  debugger
+  return tripHtml
+}
+
+Trip.prototype.tripTakenDisplay = function() {
+  let tripHtml = `
+  <h2> Activity Name: ${this.activity.name} </h2>
+  <div class="info">
+  <h3> Activity Category: ${this.activity.category}</h3>
+  <h3> Distance: ${this.activity.distance}</h3>
+  <h3> Address: ${this.activity.city}, ${this.activity.state}</h3>
+  <h2> ${this.user.name}'s Trip:</h2>
+  <h3> Date: ${this.date} </h3>
+  <h3> Time: ${this.time_ellapsed} hours</h3>
+  <h3> User Comments: ${this.comment}  </h3>
+  </div>
+  <a class="button" href="/activities/${this.activity_id}/trips">See similar Trips taken by other Users</a>
+  <a class="button" href="/trips/${this.id}/edit">Edit Trip Details</a>
+  <br>
+  <a class="button" href="/trip/${this.id}/delete">Delete Trip</a>
+  </div>
+  `
   return tripHtml
 }
